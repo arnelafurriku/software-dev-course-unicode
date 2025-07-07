6e7b4e1df08c71074a5152d6d5fd1044d1a136da
Arnela Cuci <arnelafurriku@gmail.com>
2025-07-06 23:10:58 -0500

    Complete Unicode

--- unicode.js
+++ unicode.js
@@ -86,15 +86,18 @@ Assign the result to a variable named swappedString.
 //Starter Code
 // Task 1
 let inputString1 = "Code";
-let firstCodePoint; // Your code here
-let thirdCodePoint; // Your code here
+let firstCodePoint = inputString1.charCodeAt(0);
+let thirdCodePoint = inputString1.charCodeAt(2);
 
 // Task 2
-let wordFromCodePoints; // Your code here
+let wordFromCodePoints  = String.fromCharCode(72, 101, 108, 108);
 
 // Task 3
 let inputString2 = "Launch";
-let swappedString; // Your code here
+let firstChar = inputString2.charAt(0); 
+let lastChar = inputString2.charAt(inputString2.length - 1);
+let middle = inputString2.slice(1, -1);
+swappedString = lastChar + middle + firstChar;
 
 // Log all results
 console.log({
